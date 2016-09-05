a471455219b903ee47de4728ec18bdc53db0f547
xiestrikerzj <strikerzj@163.com>
2016-09-05 10:03:05 +0800

    jquery abled

--- main.js
+++ main.js
@@ -10,22 +10,22 @@ let mainWindow;
 
 function createWindow() {
   // 创建浏览器窗口。
-    mainWindow = new BrowserWindow({width: 800, height: 600});
+  mainWindow = new BrowserWindow({width: 800, height: 600});
 
       // 加载应用的 index.html。
         mainWindow.loadURL(`file://${__dirname}/index.html`);
 
 	  // 启用开发工具。
-	    mainWindow.webContents.openDevTools();
+	  mainWindow.webContents.openDevTools();
 
 	      // 当 window 被关闭，这个事件会被触发。
-	        mainWindow.on('closed', () => {
+	      mainWindow.on('closed', () => {
 		    // 取消引用 window 对象，如果你的应用支持多窗口的话，
 		        // 通常会把多个 window 对象存放在一个数组里面，
 			    // 与此同时，你应该删除相应的元素。
-			        mainWindow = null;
-				  });
-				  }
+			    mainWindow = null;
+			});
+	  }
 
 				  // Electron 会在初始化后并准备
 				  // 创建浏览器窗口时，调用这个函数。
@@ -36,18 +36,19 @@ function createWindow() {
 				  app.on('window-all-closed', () => {
 				    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
 				      // 否则绝大部分应用及其菜单栏会保持激活。
-				        if (process.platform !== 'darwin') {
-					    app.quit();
-					      }
-					      });
+				      if (process.platform !== 'darwin') {
+				      	app.quit();
+				      }
+				  });
 
-					      app.on('activate', () => {
+				  app.on('activate', () => {
 					        // 在 macOS 上，当点击 dock 图标并且该应用没有打开的窗口时，
 						  // 绝大部分应用会重新创建一个窗口。
-						    if (mainWindow === null) {
-						        createWindow();
-							  }
-							  });
+						  if (mainWindow === null) {
+						  	createWindow();
+						  }
+						});
 
 							  // 在这文件，你可以续写应用剩下主进程代码。
 							  // 也可以拆分成几个文件，然后用 require 导入。
+
