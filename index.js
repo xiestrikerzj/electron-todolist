c999232f084b4f1f02c88b69f7133328061269db
xiestrikerzj <strikerzj@163.com>
2016-09-05 21:42:42 +0800

    main input added

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 $(function(){
-  console.log($('h1'));
+  window.Fn = {
+    getDatabase(){
+      if (!window.indexedDB) {  
+        window.indexedDB = window.mozIndexedDB || window.webkitIndexedDB;  
+      }  
+
+      var request = indexedDB.open("MyTestDatabase");  
+
+      request.onsuccess = function(e) {  
+        // Obtain IDBDatabase   
+        // VERSION_CHANGE transaction callback  
+        console.log(1);
+        var db = request.result;  
+      }  
+    },
+    createDatabase(dbName) {
+     var openRequest = localDatabase.indexedDB.open(dbName);
+
+     openRequest.onerror = function(e) {
+      console.log("Database error: " + e.target.errorCode);
+    };
+    openRequest.onsuccess = function(event) {
+      console.log("Database created");
+      localDatabase.db = openRequest.result;
+    };
+    openRequest.onupgradeneeded = function (evt) {
+
+    };
+  },
+  getData(){
+
+  }
+}
 })
