c0c6fd0676152fe2f306b9cedd4aa8c8007c6673
xieyong <xieyong@mogujie.com>
2016-09-22 12:18:24 +0800

    indexeddb created

--- index.js
+++ index.js
@@ -1,35 +1,59 @@
-$(function(){
-  window.Fn = {
-    getDatabase(){
-      if (!window.indexedDB) {  
-        window.indexedDB = window.mozIndexedDB || window.webkitIndexedDB;  
-      }  
+$(function () {
+    window.Common = {};
 
-      var request = indexedDB.open("MyTestDatabase");  
+    window.Fn = {
+        init(){
+            Fn.openDB(Conf.mainDBName);
 
-      request.onsuccess = function(e) {  
-        // Obtain IDBDatabase   
-        // VERSION_CHANGE transaction callback  
-        console.log(1);
-        var db = request.result;  
-      }  
-    },
-    createDatabase(dbName) {
-     var openRequest = localDatabase.indexedDB.open(dbName);
+            Fn.addData({test: 1});
+        },
+        openDB (name, version=2) {
+            var version = version || 1;
+            var request = window.indexedDB.open(name, version);
+            request.onerror = function (e) {
+                console.log(e.currentTarget.error.message);
+            };
+            request.onsuccess = function (e) {
+                Common.mainDB = e.target.result;
+            };
+            request.onupgradeneeded = function (e) {
+                var db = e.target.result;
+                if (!db.objectStoreNames.contains(Conf.mainStoreName)) {
+                    db.createObjectStore(Conf.mainStoreName, {keyPath: "id"});
+                }
+                console.log('DB version changed to ' + version);
+            };
+        },
+        closeDB(db){
+            db.close();
+        },
+        deleteDB(dbName){
+            indexedDB.deleteDatabase(dbName);
+        },
+        getDataByKey(db, storeName, value){
+            var transaction = db.transaction(storeName, 'readwrite');
+            var store = transaction.objectStore(storeName);
+            var request = store.get(value);
+            request.onsuccess = function (e) {
+                var student = e.target.result;
+                console.log(student.name);
+            };
+        },
+        addData(datas, storeName = Conf.mainStoreName, db = Common.mainDB){
+            datas = [].concat(datas);
+            var transaction = db.transaction(storeName, 'readwrite');
+            var store = transaction.objectStore(storeName);
 
-     openRequest.onerror = function(e) {
-      console.log("Database error: " + e.target.errorCode);
-    };
-    openRequest.onsuccess = function(event) {
-      console.log("Database created");
-      localDatabase.db = openRequest.result;
-    };
-    openRequest.onupgradeneeded = function (evt) {
+            for (var i = 0, len = datas.length; i < len; i++) {
+                store.add(datas[i]);
+            }
+        }
+    }
 
-    };
-  },
-  getData(){
+    window.Conf = {
+        mainDBName: 'todolistDB',
+        mainStoreName: 'todolistStore',
+    }
 
-  }
-}
+    Fn.init();
 })
